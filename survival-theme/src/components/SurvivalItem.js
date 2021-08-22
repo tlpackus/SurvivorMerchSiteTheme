1d05a37cc6b6a16faed5c13b57b10e1affb35488
tlpackus <tlpackus@gmail.com>
2021-08-22 10:27:30 -0700

    add propps and proptypes to SurvivalItem component

--- survival-theme/src/components/SurvivalItem.js
+++ survival-theme/src/components/SurvivalItem.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function SurvivalItem(){
+function SurvivalItem(props){
   return (
     <React.Fragment>
-      <h1>Survival Gear</h1>
+      <h2>{props.name}</h2>
+      <p>{props.image}</p>
+      <p><em>{props.description}</em></p>
+      <p>{props.quantity}</p>
+      <p>{props.cost}</p>
       <h3>Bear Grills Uses this!</h3>
       <hr/>
     </React.Fragment>
   );
 }
+
+SurvivalItem.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.image,
+  description: PropTypes.string,
+  quantity: PropTypes.number,
+  cost: PropTypes.number
+};
 
 export default SurvivalItem;
