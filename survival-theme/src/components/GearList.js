8553e5b11a8f64dc7a6fb5fdca756d4c65eab500
tlpackus <tlpackus@gmail.com>
2021-08-23 16:44:14 -0700

    add masterGearList and use .map in the GearList() to update the dom with its data

--- survival-theme/src/components/GearList.js
+++ survival-theme/src/components/GearList.js
@@ -1,19 +1,32 @@
 import React from "react";
 import SurvivalItem from "./SurvivalItem.js"
 
+const masterGearList = [
+  {
+    name: "Survival Watch",
+    description: "compass, watch, and fire starter",
+    quantity:  "10",
+    cost:  "5"
+  },
+  {
+    name: "First Aid Kit",
+    description: "All first aid necessities",
+    quantity:  "10",
+    cost:  "10"
+  }
+];
 function GearList(){
   return (
     <React.Fragment>
-      <SurvivalItem 
-      name="Survival Watch"
-      description="compass, watch, and fire starter"
-      quantity= "10"
-      cost= "5" />
-    <SurvivalItem 
-      name="First Aid Kit"
-      description="All first aid necessities"
-      quantity= "10"
-      cost= "10" />
+      <hr/>
+      {masterGearList.map((survivalItem, index) =>
+        <SurvivalItem 
+        name={survivalItem.name}
+        description={survivalItem.description}
+        quantity={survivalItem.quantity}
+        cost={survivalItem.cost}
+        key={index}/>
+      )}
     </React.Fragment>
   );
 }
