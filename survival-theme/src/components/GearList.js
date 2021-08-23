236ff11713c070f5aabb8151d2021ed08124f1d8
tlpackus <tlpackus@gmail.com>
2021-08-23 16:27:32 -0700

    add two survival items to gera list to prove they are displaying, import porptypes, and change Img folder to img

--- survival-theme/src/components/GearList.js
+++ survival-theme/src/components/GearList.js
@@ -3,7 +3,18 @@ import SurvivalItem from "./SurvivalItem.js"
 
 function GearList(){
   return (
-    <SurvivalItem />
+    <React.Fragment>
+      <SurvivalItem 
+      name="Survival Watch"
+      description="compass, watch, and fire starter"
+      quantity= "10"
+      cost= "5" />
+    <SurvivalItem 
+      name="First Aid Kit"
+      description="All first aid necessities"
+      quantity= "10"
+      cost= "10" />
+    </React.Fragment>
   );
 }
 
--- survival-theme/src/components/SurvivalItem.js
+++ survival-theme/src/components/SurvivalItem.js
@@ -1,13 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function SurvivalItem(props){
   return (
     <React.Fragment>
       <h2>{props.name}</h2>
-      <p>{props.image}</p>
-      <p><em>{props.description}</em></p>
-      <p>{props.quantity}</p>
-      <p>{props.cost}</p>
+      <p>Description: <em>{props.description}</em></p>
+      <p>Quantity: {props.quantity}</p>
+      <p>Cost: {props.cost}</p>
       <h3>Bear Grills Uses this!</h3>
       <hr/>
     </React.Fragment>
@@ -16,7 +16,6 @@ function SurvivalItem(props){
 
 SurvivalItem.propTypes = {
   name: PropTypes.string,
-  image: PropTypes.image,
   description: PropTypes.string,
   quantity: PropTypes.number,
   cost: PropTypes.number
