3e383c6809be15b7edb7a54c81d29162e5444257
tlpackus <tlpackus@gmail.com>
2021-08-24 17:30:12 -0700

    add controller as a parent class for new gear from and gear list, creating buttons to navigate between the two components in the browser

---
+++ survival-theme/src/components/SurvivalItemControll.js
@@ -0,0 +1,37 @@
+import React from "react";
+import NewSurvivalItemForm from './NewSurvivalItemForm';
+import GearList from "./GearList";
+
+class SurvivalItemControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fromVisibleOnPage: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage}));
+  }
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewSurvivalItemForm />
+      buttonText = "Return to gear inventory"
+    } else {
+      currentlyVisibleState = <GearList />
+      buttonText = "Add New Products";
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> 
+      </React.Fragment>
+    );
+  }
+}
+
+export default SurvivalItemControl;
