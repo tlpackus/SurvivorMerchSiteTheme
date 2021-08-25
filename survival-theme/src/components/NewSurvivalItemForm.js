e3bad766871e97045f935a273a2843a9ad299b94
tlpackus <tlpackus@gmail.com>
2021-08-24 17:25:30 -0700

    add New gear test form template to make sure i can connect it properly

---
+++ survival-theme/src/components/NewSurvivalItemForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewSurvivalItemForm(props){
+  return (
+    <React.Fragment>
+      <h3>This is the test from.</h3>
+    </React.Fragment>
+  )
+}
+
+export default NewSurvivalItemForm;
