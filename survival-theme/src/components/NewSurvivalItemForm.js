7874aded56db08241b1e3aca3d25bacb010c5b42
tlpackus <tlpackus@gmail.com>
2021-08-28 19:19:30 -0700

    add the inputs for a new survival item submission with prop types specified and a handles to submit the gathred data

--- survival-theme/src/components/NewSurvivalItemForm.js
+++ survival-theme/src/components/NewSurvivalItemForm.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { v4 } from 'uuid';  
+import PropTypes from "prop-types"; 
 
 function NewSurvivalItemForm(props){
   return (
     <React.Fragment>
-      <h3>This is the test from.</h3>
+      <form onSubmit={handleNewSurvivalItemFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Item Name' />
+        <input
+          type='text'
+          name='description'
+          placeholder='Description' />
+        <input
+          type='number'
+          name='quantity'
+          placeholder='Quantity' />
+        <input
+          type='number'
+          name='cost'
+          placeholder='Cost' />
+        <button type='submit'>Add item to marketplace</button>
+      </form>
     </React.Fragment>
   )
+  function handleNewSurvivalItemFormSubmission(event) {
+    event.preventDefault();
+    props.onNewSurvivalItemCreation({name: event.target.name.value, description: event.target.description.value, quantity: parseInt(event.target.quantity.value), cost: parseInt(event.target.cost.value), id: v4()});
+  }
 }
+
+NewSurvivalItemForm.propTypes = {
+  onNewSurvivalItemCreation: PropTypes.func
+};
 
 export default NewSurvivalItemForm;
